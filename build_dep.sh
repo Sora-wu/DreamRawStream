35ac802f91be8a26996d0da0f6a0a15d5bb177c3
Sora-wu <2466201641@qq.com>
2026-09-01 00:32:29 +0800

    move DreamNet submodule into third/src

--- build_dep.sh
+++ build_dep.sh
@@ -1,7 +1,7 @@
 rm -rf third/build/linux
 
-cd DreamNet
+cd third/src/DreamNet
 rm -rf build
-cmake -B build -DCMAKE_INSTALL_PREFIX=../third/build/linux -DCMAKE_BUILD_TYPE=Debug
+cmake -B build -DCMAKE_INSTALL_PREFIX=../../build/linux -DCMAKE_BUILD_TYPE=Debug
 cmake --build build -j
 cmake --install build
